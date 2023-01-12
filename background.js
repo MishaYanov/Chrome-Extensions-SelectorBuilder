0633d6841e6b4d4e03e758d075fb5663fd2f2f63
MishaYanov <86347940+MishaYanov@users.noreply.github.com>
2023-01-12 22:19:59 +0200

    changes scripts, add html

--- background.js
+++ background.js
@@ -1,26 +1,8 @@
-document.getElementById("select-element-button").addEventListener("click", function() {
-    chrome.tabs.executeScript({
-      file: 'content_script.js'
-    });
-  });
-
-
-
-let selectedElement;
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.method == "getSelectedElement") {
-      sendResponse({data: selectedElement});
-    }
-    else if (request.method == "setSelectedElement") {
-      selectedElement = request.data;
-      sendResponse({status: "success"});
-    }
-  });
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    file: 'content_script.js'
+chrome.action.onClicked.addListener((tab) => {
+  debugger
+  console.log('tab', tab)
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
   });
 });
